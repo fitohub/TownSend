0d63f1a388ea6202000a4c8e9403a4bd92c09168
fernandoito <fernando.ito.anywhere@gmail.com>
2019-08-15 01:40:54 -0400

    Removing remaining comments.

--- src/appModules/DeleteAction.java
+++ src/appModules/DeleteAction.java
@@ -3,8 +3,6 @@ package appModules;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-
 import pageObjects.HomePage;
 import utility.Log;
 
